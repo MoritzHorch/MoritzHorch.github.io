eb5cdb178912878a860e872bc46c65ac3fbce460
= <=>
2022-08-09 13:12:21 +0200

    Setup vitest for component and unit testing

---
+++ test/App.unit.test.ts
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/svelte';
+import App from 'src/App.svelte';
+
+describe('App ...', () => {
+  it('should render Hello World', () => {
+    render(App);
+
+    const helloWorldText = screen.queryByText('Hello World');
+
+    expect(helloWorldText).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte({
+      hot: !process.env.VITEST
+    })
+  ],
+  test: {
+    setupFiles: ['test/setupTests.ts'],
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      all: true,
+      include: ['src/**'],
+      exclude: ['src/main.ts', 'src/**/*.d.ts'],
+      reporter: ['text', 'lcov']
+    }
+  }
 });
